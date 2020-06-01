f12f4036d2f8302570cdf19cff2bd3d654589bf9
Miquel de Domingo <miquelddg@gmail.com>
2020-06-01 23:35:25 +0200

    Separated textStyle & textTheme

--- lib/ui/theme_data.dart
+++ lib/ui/theme_data.dart
@@ -3,6 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 enum AppTheme { Dark, Light }
 
+final TextTheme textTheme = GoogleFonts.workSansTextTheme();
+final TextStyle textStyle = GoogleFonts.workSans();
+
+
 final Map<AppTheme, ThemeData> appThemeData = {
   AppTheme.Dark: ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Color(0xFF191924),
@@ -26,57 +30,57 @@ final Map<AppTheme, ThemeData> appThemeData = {
         color: Colors.white,
       ),
     ),
-    textTheme: GoogleFonts.workSansTextTheme().copyWith(
+    textTheme: textTheme.copyWith(
       // Screen header
-      headline1: GoogleFonts.workSans().copyWith(
+      headline1: textStyle.copyWith(
         color: Colors.white,
         fontSize: 28.0,
         fontWeight: FontWeight.bold,
         letterSpacing: 1,
       ),
       // Weight tile
-      headline2: GoogleFonts.workSans().copyWith(
+      headline2: textStyle.copyWith(
         color: Colors.white,
         fontWeight: FontWeight.bold,
         fontSize: 32.0,
       ),
       // Minor titles
-      headline3: GoogleFonts.workSans().copyWith(
+      headline3: textStyle.copyWith(
         color: Colors.white,
         fontWeight: FontWeight.bold,
         fontSize: 28.0,
       ),
-      headline4: GoogleFonts.workSans().copyWith(
+      headline4: textStyle.copyWith(
         fontSize: 20.0,
         fontWeight: FontWeight.bold,
         letterSpacing: 1,
         color: Colors.white,
       ),
-      headline5: GoogleFonts.workSans().copyWith(
+      headline5: textStyle.copyWith(
         color: Colors.white,
         fontWeight: FontWeight.bold,
         fontSize: 16.0,
         letterSpacing: 1.0,
       ),
-      headline6: GoogleFonts.workSans().copyWith(
+      headline6: textStyle.copyWith(
         color: Colors.orange,
         fontWeight: FontWeight.bold,
         fontSize: 16.0,
         letterSpacing: 1.0,
       ),
-      bodyText1: GoogleFonts.workSans().copyWith(
+      bodyText1: textStyle.copyWith(
         color: Colors.white,
         fontSize: 14.0,
         letterSpacing: 0.5,
       ),
       // BMI Tile, Calendar header...
-      subtitle1: GoogleFonts.workSans().copyWith(
+      subtitle1: textStyle.copyWith(
         fontWeight: FontWeight.bold,
         color: Color(0xFF8a8b98),
         letterSpacing: 0.5,
         fontSize: 20.0,
       ),
-      button: GoogleFonts.workSans().copyWith(
+      button: textStyle.copyWith(
         color: Color(0xFF191924),
         fontWeight: FontWeight.bold,
         letterSpacing: 1.0,
